51dde822831ee9ac48eba025e1197d738cba9939
xOravec4 <tomas.oravec94@gmail.com>
2015-05-10 17:36:43 +0200

    web app - par uprav
    startLisneter

--- PB138-java-maven/src/main/java/com/mycompany/pb138/java/maven/ListServlet.java
+++ PB138-java-maven/src/main/java/com/mycompany/pb138/java/maven/ListServlet.java
@@ -1,6 +1,9 @@
 package com.mycompany.pb138.java.maven;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,18 +16,45 @@ public class ListServlet extends HttpServlet {
 
     public static final String URL_MAPPING = "/list";
     private static final String LIST_JSP = "/grammarList.jsp";
+    private static final Logger log = Logger.getLogger(ListServlet.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            GrammarManager gm = new GrammarManager("ourXMLDatabse.xml");
-            gm.loadGrammarsFromXML();
-            
-            request.setAttribute("grammarList", gm.getGrammars());
+        try 
+        {
+            request.setAttribute("grammarList", ((GrammarManager) getServletContext().getAttribute("GrammarManager")).getGrammars());
             request.getRequestDispatcher(LIST_JSP).forward(request, response);
         } catch (ServletException | IOException e) {
-            //log.log(Level.SEVERE, "Error cannot show cellars", e);
+            log.log(Level.SEVERE, "Error, cannot show grammars.", e);
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        String action = request.getPathInfo();
+
+        switch (action) {
+            case "/add":
+                try {
+                    String name = request.getParameter("name");
+                    List<Rule> rules = null; //TODO
+
+                    Grammar newGrammar = new Grammar(name);
+                    for (Rule rule : rules) {
+                        newGrammar.addRule(rule);
+                    }
+                    ((GrammarManager) getServletContext().getAttribute("GrammarManager")).addGrammar(newGrammar);
+                } catch (Exception e) {
+                    log.log(Level.SEVERE, "Cannot add Grammar.", e);
+                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                    return;
+                }
+            default:
+                log.log(Level.WARNING, "Unknown action {0}", action);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Unknown action " + action);
         }
     }
 
